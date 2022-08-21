9ed31a5dae9fc8895ef848acfaebd057a11a68d2
PatrykRomanowski <71602577+PatrykRomanowski@users.noreply.github.com>
2022-08-21 22:37:02 +0200

    add bars

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import classes from "./header.module.css";
 
 import pad from "../assets/pad2.jpg";
+import switchIcon from "../assets/icons/switch.png";
+import dsIcon from "../assets/icons/DS.png";
+import wiiIcon from "../assets/icons/WII.png";
+import allConsole from "../assets/icons/allConsole.png";
 
 const Header = () => {
+  const [gameCounter, setGameCounter] = useState(0);
+
+  useEffect(() => {
+    setGameCounter(20);
+  }, []);
+
   return (
     <div className={classes.header}>
-      <img src={pad}></img>
-      <p>Header</p>
+      <img className={classes.mainImage} src={pad}></img>
+      <div className={classes.chart}>
+        <p>My game status: {gameCounter} </p>
+        <div className={classes.barConteiner}>
+          <img className={classes.icon} src={switchIcon}></img>
+          <p className={classes.nameConsole}>SWITCH </p>
+          <div className={classes.bar}></div>
+        </div>
+        <div className={classes.barConteiner}>
+          <img className={classes.icon} src={dsIcon}></img>
+          <p className={classes.nameConsole}>3DS </p>
+          <div className={classes.bar}></div>
+        </div>
+        <div className={classes.barConteiner}>
+          <img className={classes.icon} src={wiiIcon}></img>
+          <p className={classes.nameConsole}>WII </p>
+          <div className={classes.bar}>
+            <div className={classes.fillBar}></div>
+          </div>
+        </div>
+        <div className={classes.barConteiner}>
+          <img className={classes.icon} src={allConsole}></img>
+          <p className={classes.nameConsole}>OTHER </p>
+          <div className={classes.bar}></div>
+        </div>
+      </div>
     </div>
   );
 };
